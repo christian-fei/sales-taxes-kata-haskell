e6261594ec3449bbbd26f43b051985aac87e808b
Christian Fei <crifei93@gmail.com>
2015-02-18 21:39:37 +0100

    extracted calc_basic_sales_tax method

--- src/SalesTaxes.hs
+++ src/SalesTaxes.hs
@@ -9,4 +9,8 @@ import Good
 basic_sales_tax = 0.1
 
 priceFor :: Good -> Double
-priceFor (Good {price = p}) = fromIntegral (round (100 *((fromIntegral $ round (p * basic_sales_tax * 20)) / 20 + p))) / 100
+priceFor g@(Good {price = p}) = calc_basic_sales_tax g
+
+calc_basic_sales_tax :: Good -> Double
+calc_basic_sales_tax (Good {price = p}) =
+  fromIntegral (round (100 *((fromIntegral $ round (p * basic_sales_tax * 20)) / 20 + p))) / 100
