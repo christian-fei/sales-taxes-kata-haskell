70c916de01567a554dba49864e05cbdb5ce15482
Christian Fei <crifei93@gmail.com>
2015-02-18 22:33:02 +0100

    added test for imported goods

--- src/SalesTaxes.test.hs
+++ src/SalesTaxes.test.hs
@@ -8,14 +8,19 @@ main = do runTestTT tests
 
 tests = TestList [
   calculatesPriceForGoodWithBasicSalesTax,
-  calculatesPriceForGoodExemptOfBasicSalesTax
+  calculatesPriceForGoodExemptOfBasicSalesTax,
+  calculatesPriceForImportedGoodExemptOfBasicSalesTax
   ]
 
 good = Good {price = 14.99, kind = Electronics, imported = False}
 basicTaxExemptGood = Good {price = 12.49, kind = Book, imported = False}
+importedGood = Good {price = 10.00, kind = Food, imported = True}
 
 calculatesPriceForGoodWithBasicSalesTax =
   priceFor good ~?= 16.49
 
 calculatesPriceForGoodExemptOfBasicSalesTax =
   priceFor basicTaxExemptGood ~?= 12.49
+
+calculatesPriceForImportedGoodExemptOfBasicSalesTax =
+  priceFor importedGood ~?= 10.50
