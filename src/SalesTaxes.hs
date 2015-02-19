06e3ad972a3d89b099f8b08ce1844932f0cfde60
Christian Fei <crifei93@gmail.com>
2015-02-19 18:27:30 +0100

    extracted duplicate calculation logic for taxes in calc_tax func

--- src/SalesTaxes.hs
+++ src/SalesTaxes.hs
@@ -14,11 +14,13 @@ priceFor g@(Good {price = p, kind = k})
   | k `elem` [Food,Book,Medicine]  = price g + calc_imported_tax g
   | otherwise        =  price g + calc_basic_tax g + calc_imported_tax g
 
+calc_tax :: Double -> Double -> Double
+calc_tax price tax = fromIntegral (round (100 *((fromIntegral $ round (price * tax * 20)) / 20))) / 100
+
 calc_basic_tax :: Good -> Double
-calc_basic_tax (Good {price = p}) =
-  fromIntegral (round (100 *((fromIntegral $ round (p * basic_sales_tax * 20)) / 20))) / 100
+calc_basic_tax (Good {price = p}) = calc_tax p basic_sales_tax
 
 calc_imported_tax :: Good -> Double
 calc_imported_tax (Good {price = p, imported = i})
-  | i == True  = fromIntegral (round (100 *((fromIntegral $ round (p * imported_tax * 20)) / 20))) / 100
+  | i == True  = calc_tax p imported_tax
   | otherwise  = 0
